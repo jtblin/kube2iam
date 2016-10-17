b13397d9068c95a4a50fa16da68e538ce6e4cff1
Denis Dzyubenko <denis.dzyubenko@schibsted.com>
2016-10-17 15:29:11 +0200

    Prepend iptables rule instead of appending it
    This fixes an issue in Kubernetes when the appended rule is never activated
    because it is swalled by pre-existing rules.

--- iptables/iptables.go
+++ iptables/iptables.go
@@ -6,6 +6,21 @@ import (
 	"github.com/coreos/go-iptables/iptables"
 )
 
+func iptablesInsertUnique(ipt *iptables.IPTables, table, chain string, pos int, rulespec ...string) error {
+	exists, err := ipt.Exists(table, chain, rulespec...)
+	if err != nil {
+		return err
+	}
+
+	if exists {
+		if err := ipt.Delete(table, chain, rulespec...); err != nil {
+			return err
+		}
+	}
+
+	return ipt.Insert(table, chain, pos, rulespec...)
+}
+
 // AddRule adds the required rule to the host's nat table
 func AddRule(appPort, metadataAddress, hostInterface, hostIP string) error {
 	if hostIP == "" {
@@ -17,8 +32,8 @@ func AddRule(appPort, metadataAddress, hostInterface, hostIP string) error {
 		return err
 	}
 
-	if err := ipt.AppendUnique(
-		"nat", "PREROUTING", "-p", "tcp", "-d", metadataAddress, "--dport", "80",
+	if err := iptablesInsertUnique(ipt,
+		"nat", "PREROUTING", 1, "-p", "tcp", "-d", metadataAddress, "--dport", "80",
 		"-j", "DNAT", "--to-destination", hostIP+":"+appPort, "-i", hostInterface,
 	); err != nil {
 		return err

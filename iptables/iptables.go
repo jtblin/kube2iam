d55bfe9e275279d7d5244da42134b402b7025354
John Torres <enfermo337@yahoo.com>
2016-12-04 16:38:33 -0500

    Fail on invalid host interface (#34)
    * throw error if hostInterface does not exist
    
    * test iptables package
    
    * refactor based feedback from @jtblin and linting
    
    * clean-up of test cases

--- iptables/iptables.go
+++ iptables/iptables.go
@@ -2,12 +2,18 @@ package iptables
 
 import (
 	"errors"
+	"net"
 
 	"github.com/coreos/go-iptables/iptables"
 )
 
 // AddRule adds the required rule to the host's nat table
 func AddRule(appPort, metadataAddress, hostInterface, hostIP string) error {
+
+	if err := CheckInterfaceExists(hostInterface); err != nil {
+		return err
+	}
+
 	if hostIP == "" {
 		return errors.New("--host-ip must be set")
 	}
@@ -25,4 +31,10 @@ func AddRule(appPort, metadataAddress, hostInterface, hostIP string) error {
 	}
 
 	return nil
+}
+
+// CheckInterfaceExists - validates the interface passed exists for the given system
+func CheckInterfaceExists(hostInterface string) error {
+	_, err := net.InterfaceByName(hostInterface)
+	return err
 }
---
+++ iptables/iptables_test.go
@@ -0,0 +1,34 @@
+package iptables
+
+import (
+	"runtime"
+	"testing"
+)
+
+func TestCheckInterfaceExistsFailsWithBogusInterface(t *testing.T) {
+	ifc := "bogus0"
+	if err := CheckInterfaceExists(ifc); err == nil {
+		t.Error("Should fail with invalid interface. Interface received:", ifc)
+	}
+}
+
+func TestCheckInterfaceExistsPassesWithValidInterface(t *testing.T) {
+	var ifc string
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		ifc = "lo0"
+	case "linux":
+		ifc = "lo"
+	default:
+		// everything else that we don't know or care about...fail
+		ifc = "unknown"
+		t.Error("%s OS '%s'\n", ifc, os)
+	}
+	if err := CheckInterfaceExists(ifc); err != nil {
+		t.Error("Should pass with valid interface. Interface received:", ifc)
+	}
+}
+
+func TestAddRule(t *testing.T) {
+	t.Skip()
+}

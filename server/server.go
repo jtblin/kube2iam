jtblin/kube2iam

commit 8e543ea6517f2eba7225bcb770d65f9b0d02535f

Fix for roles with a custom path name (#86)
It's possible to scope roles to a custom path (arn:aws:iam::123456789012:role/team-a/test-role). 
The current implementation doesn't support this. Instead of test-role this fix will
return team-a/test-role within the securityCredentialsHandler handler. See
http://docs.aws.amazon.com/IAM/latest/UserGuide/reference_identifiers.html for more details
about role paths.

server/server.go
@@ -202,8 +202,7 @@ func (s *Server) securityCredentialsHandler(logger *log.Entry, w http.ResponseWr
 	// If a base ARN has been supplied and this is not cross-account then
 	// return a simple role-name, otherwise return the full ARN
 	if s.iam.BaseARN != "" && strings.HasPrefix(roleARN, s.iam.BaseARN) {
-		idx := strings.LastIndex(roleARN, "/")
-		write(logger, w, roleARN[idx+1:])
+		write(logger, w, strings.TrimPrefix(roleARN, s.iam.baseARN))
 		return
 	}
 	write(logger, w, roleARN)

commit 9275fcf93e8d2eb6e13373f0f7d405abb7dfaa96

Fix typo from @totallyunknown introduced in 8e543ea

server/server.go
@@ -204,7 +204,7 @@ func (s *Server) securityCredentialsHandler(logger *log.Entry, w http.ResponseWr
 	// If a base ARN has been supplied and this is not cross-account then
 	// return a simple role-name, otherwise return the full ARN
 	if s.iam.BaseARN != "" && strings.HasPrefix(roleARN, s.iam.BaseARN) {
-		write(logger, w, strings.TrimPrefix(roleARN, s.iam.baseARN))
+		write(logger, w, strings.TrimPrefix(roleARN, s.iam.BaseARN))
 		return
 	}
 	write(logger, w, roleARN)
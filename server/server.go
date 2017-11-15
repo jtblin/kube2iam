7dbdcb49ce3a24430eab78be3aeaef4c5f35bc0e
Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>
2017-11-15 11:33:28 +0100

    Limit relevant pods to those on the same node as kube2iam
    In clusters with 1000's of pods the memory used by each kube2iam
    instance explodes because each kube2iam instance is storing information
    about all pods on all nodes in memory.
    
    This adds a flag `--node` for specifying the node name of kube2iam. If
    set, it will limit the pods it's watching to those only on the same
    node as the kube2iam instance.

--- server/server.go
+++ server/server.go
@@ -46,6 +46,7 @@ type Server struct {
 	MetadataAddress         string
 	HostInterface           string
 	HostIP                  string
+	NodeName                string
 	NamespaceKey            string
 	LogLevel                string
 	AddIPTablesRule         bool
@@ -259,8 +260,8 @@ func write(logger *log.Entry, w http.ResponseWriter, s string) {
 }
 
 // Run runs the specified Server.
-func (s *Server) Run(host, token string, insecure bool) error {
-	k, err := k8s.NewClient(host, token, insecure)
+func (s *Server) Run(host, token, nodeName string, insecure bool) error {
+	k, err := k8s.NewClient(host, token, nodeName, insecure)
 	if err != nil {
 		return err
 	}

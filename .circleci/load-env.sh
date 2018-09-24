a25c82d1566eb80071c189221702bae3ff18880a
Jesse Stuart <hi@jessestuart.com>
2018-09-23 21:30:58 -0400

    fix(ci): Correctly determine version # for image tagging.

--- .circleci/load-env.sh
+++ .circleci/load-env.sh
@@ -7,7 +7,7 @@ echo 'export GOROOT=/usr/local/go' >> $BASH_ENV
 echo 'export IMAGE=kube2iam' >> $BASH_ENV
 echo 'export REGISTRY=jessestuart' >> $BASH_ENV
 echo 'export QEMU_VERSION=v2.12.0' >> $BASH_ENV
-echo 'export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases | jq -r "sort_by(.tag_name)[-1].tag_name")' >> $BASH_ENV
+echo 'export VERSION=$(curl -s https://api.github.com/repos/jtblin/kube2iam/releases/latest | jq -r ".tag_name")' >> $BASH_ENV
 echo 'export PATH="/usr/local/go/bin:/root/go/bin:/root/go/bin/linux_${GOARCH}/:$PATH"' >> $BASH_ENV
 echo 'export GO_REPO=jtblin/kube2iam' >> $BASH_ENV
 

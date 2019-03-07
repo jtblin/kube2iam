49a5e5dcae17e1b9c2b245600f9d67f03355d700
Peter Rifel <pgrifel@gmail.com>
2019-03-07 09:34:26 -0800

    Do not check the pod's deletionTimestamp
    This was causing nil entries in the pod index which resulted in credentials failing to be issued.
    See #178 for more information.

--- pod.go
+++ pod.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	log "github.com/sirupsen/logrus"
-	"k8s.io/client-go/pkg/api/unversioned"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/cache"
 )
@@ -72,14 +71,9 @@ func (p *PodHandler) OnDelete(obj interface{}) {
 }
 
 func isPodActive(p *v1.Pod) bool {
-	podDeleted := false
-	if p.DeletionTimestamp != nil {
-		podDeleted = p.DeletionTimestamp.Before(unversioned.Now())
-	}
 	return p.Status.PodIP != "" &&
 		v1.PodSucceeded != p.Status.Phase &&
-		v1.PodFailed != p.Status.Phase &&
-		!podDeleted
+		v1.PodFailed != p.Status.Phase
 }
 
 // PodIPIndexFunc maps a given Pod to it's IP for caching.

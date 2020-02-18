d5d617eec221111537dd74b053b6fae92075fe09
Luigi Tagliamonte <luigi.tagliamonte@doordash.com>
2020-02-18 14:44:13 -0800

    move error definition

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -119,8 +119,6 @@ func (k8s *Client) PodByIP(IP string) (*v1.Pod, error) {
 // If the indexed pods all have HostNetwork = true the function return nil and the error message.
 // If we retrive a running pod that doesn't have HostNetwork = true and it is in Running state will return that.
 func dealWithDuplicatedIP(k8s *Client, IP string) (*v1.Pod, error) {
-	error := fmt.Errorf("more than a pod with the same IP has been indexed, this can happen when pods have hostNetwork: true")
-
 	runningPodList, err := k8s.CoreV1().Pods("").List(v1.ListOptions{
 		FieldSelector: selector.OneTermEqualSelector("status.podIP", IP).String(),
 	})
@@ -134,6 +132,7 @@ func dealWithDuplicatedIP(k8s *Client, IP string) (*v1.Pod, error) {
 			return &pod, nil
 		}
 	}
+	error := fmt.Errorf("more than a pod with the same IP has been indexed, this can happen when pods have hostNetwork: true")
 	return nil, error
 }
 
